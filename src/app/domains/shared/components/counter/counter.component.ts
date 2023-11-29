78b179a32e530f0327a1410c5d7a727134b872da
Cristian Sanchez <cristian.sanchez.fs@outlook.com>
2023-11-28 18:37:29 -0600

    Ecommerce - Angular

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,75 @@
+import {
+  Component,
+  Input,
+  SimpleChange,
+  SimpleChanges,
+  signal,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+})
+export class CounterComponent {
+  @Input({ required: true }) duration: number = 0;
+  @Input({ required: true }) message: string = '';
+  counter = signal(0);
+  counterRef: number | undefined;
+
+  constructor() {
+    // No Async
+    // Before Render
+    // Run Once
+    console.log('constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // Before and during render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+    const duration = changes['duration'];
+    if (duration && duration.currentValue !== duration.previousValue) {
+      this.doSomething();
+    }
+  }
+
+  ngOnInit() {
+    // After render
+    // Una vez
+    // Async, then, subs
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+
+    this.counterRef = window.setInterval(() => {
+      console.log('run interval');
+      this.counter.update((statePrev) => statePrev + 1);
+    }, 1000);
+  }
+
+  ngAfterViewInit() {
+    // After render
+    // If the child of this parent was already renderize
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy() {
+    // When the document are destroyed
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef);
+  }
+
+  doSomething() {
+    console.log('changeDuration');
+    //  Async or not
+  }
+}
